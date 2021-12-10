3f266edf0e21ac687987b7bc942f849bdc4ea4b8
dgebretsadik <gebredanab@gmail.com>
2021-12-10 17:24:01 -0600

    fix: annotation add Retailer and Product Repository

--- src/main/java/com/example/onlineshopping/repository/ProductRepository.java
+++ src/main/java/com/example/onlineshopping/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.onlineshopping.repository;
 
 import com.example.onlineshopping.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/example/onlineshopping/repository/RetailerRepository.java
+++ src/main/java/com/example/onlineshopping/repository/RetailerRepository.java
@@ -2,6 +2,8 @@ package com.example.onlineshopping.repository;
 
 import com.example.onlineshopping.domain.Retailer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RetailerRepository extends JpaRepository<Retailer, Long> {
 }
